4981abae245b641c97fddc21332f4434d1481e68
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-17 13:35:37 +0530

    update all folder

--- LinkedList/Easy/Palindrome Linked List/Code.java
+++ LinkedList/Easy/Palindrome Linked List/Code.java
@@ -1,8 +1,38 @@
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null ){
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = foundmid(head);
+        // reverse
+
+        ListNode prev = null;
+        ListNode curr = mid;
+        ListNode next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next; 
+        }
+
+        ListNode right = prev;  // right half head
+        ListNode left = head;  // left first head
+
+
+        while(right != null){
+            if(left.val != right.val){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        
+        return true;
+        
+    }
+    public static ListNode foundmid(ListNode data){
+        ListNode slow = data;
+        ListNode fast = data;
+
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
