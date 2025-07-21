e51606bc9d84e0ad43e7ed39ce3ff008f5377a7e
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-07-21 10:05:59 +0530

    update Q

--- LinkedList/Medium/Delete the Middle Node of a Linked List/code.java
+++ LinkedList/Medium/Delete the Middle Node of a Linked List/code.java
@@ -11,24 +11,31 @@
 
 class code {
   public ListNode deleteMiddle(ListNode head) {
+
     int size = 0;
     ListNode temp = head;
     while (temp != null) {
       size++;
       temp = temp.next;
     }
+
     int deletenode = size / 2;
+
     if (size == 1) {
       return null;
     }
+
     ListNode prev;
     int currsize = 1;
     ListNode curr = head;
+
     while (currsize < deletenode) {
       currsize++;
       curr = curr.next;
     }
+
     curr.next = curr.next.next;
+    
     return head;
 
   }
