07d24b6d97f2349ae3d2c0402a745611f620687f
Sitaram8472 <sitaramsahu8472@gmail.com>
2025-08-01 23:20:38 +0530

    Odd Even Linked List

--- LinkedList/Medium/Odd Even Linked List/code.java
+++ LinkedList/Medium/Odd Even Linked List/code.java
@@ -1,5 +1,34 @@
-package LinkedList.Medium.Odd Even Linked List;
 
-public class code {
-  
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class code {
+  public ListNode oddEvenList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode odd = head;
+    ListNode even = head.next;
+    ListNode evenhead = even;
+
+    while (even != null && even.next != null) {
+      odd.next = even.next;
+      odd = odd.next;
+
+      even.next = odd.next;
+      even = even.next;
+    }
+
+    odd.next = evenhead;
+    return head;
+
+  }
 }
